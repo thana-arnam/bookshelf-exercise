cd6b213605225860d298b2da61b6761d6030d0fc
Thoritie <thohirh9@gmail.com>
2023-01-23 16:04:47 +0700

    :art: bring nav to layout :snowman:

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,16 +1,17 @@
 import './globals.css'
 import { K2D } from "@next/font/google";
+import Navbar from "@/app/navbar";
+import styles from "@/app/page.module.css";
 const k2d = K2D({weight: "400", subsets: ['latin']})
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body className={k2d.className}>{children}</body>
-    </html>
+      <html lang="en">
+        <head/>
+        <body className={k2d.className}>
+            <Navbar/>
+            <main className={styles.main}>{children}</main>
+        </body>
+      </html>
   )
 }
