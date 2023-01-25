2a19e141a9cad94583598b66f540b98f1f1469a1
bazsup <bas@odds.team>
2023-01-25 16:14:58 +0700

    add component mistake

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,20 @@
+import Quiz1 from "../components/Quiz1"
+import Quiz2 from "../components/Quiz2"
+import Quiz3 from "../components/Quiz3"
+import Quiz4 from "../components/Quiz4"
+
 const HomePage = () => {
-    return "Hello"
+    return (
+        <>
+            <b>Instruction:</b> Open file <code>pages/index.jsx</code>
+            {/* Uncomment the code below to start quiz */}
+            
+            {/* <Quiz1 /> */}
+            {/* <Quiz2 /> */}
+            {/* <Quiz3 /> */}
+            {/* <Quiz4 /> */}
+        </>
+    )
 }
 
 export default HomePage
