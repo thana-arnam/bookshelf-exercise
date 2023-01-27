32b49be1e6bbd3182c5fb4aa5808934f885bb685
Thoritie <thohirh9@gmail.com>
2023-01-27 14:06:26 +0700

    :zap: Replace action when click book to open book detail modal :snowman:

--- src/components/book.jsx
+++ src/components/book.jsx
@@ -1,15 +1,27 @@
 import {Playfair_Display} from "@next/font/google";
+import useModal from "@/hooks/useModal";
+import BookDetail from "@/components/BookDetail";
 
 const playfairDisplaySemiBold = Playfair_Display({weight: "600", subsets: ['latin']})
 
-export default function Book({title, finished, addNewBook}) {
+export default function Book({title, finished, description, category}) {
+    const bookDetailModal = useModal();
+
     return (
         <>
-            <div className="book" onClick={addNewBook}>
+            <div className="book" onClick={bookDetailModal.open}>
                 <img src="https://via.placeholder.com/269x355" alt="book cover"/>
                 <p className={playfairDisplaySemiBold.className + " bookName"}>{title}</p>
                 {finished? <span className="readingStatus">Finish</span>: null}
             </div>
+
+            <BookDetail
+                isShow={bookDetailModal.isShow}
+                close={bookDetailModal.close}
+                title={title}
+                description={description}
+                category={category}
+            />
         </>
     )
 }
