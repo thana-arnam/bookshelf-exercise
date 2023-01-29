3d09f1d6c7a6e70ea2aa48e0333d68b2e8135a6b
thanamat <thanamat@odds.team>
2023-01-29 08:09:36 +0000

    add state title

--- src/components/AddBookForm.jsx
+++ src/components/AddBookForm.jsx
@@ -1,12 +1,17 @@
+"use client";
+
+import React from "react";
+
 const AddBookForm = () => {
-  const title = "Domain Driven Design";
+  const [title, setTitle] = React.useState("");
+
   return (
     <div>
       <h1>ADD TO COLLECTION</h1>
       <form>
         <div>
           <label>Title </label>
-          <input value={title} />
+          <input value={title} onChange={(e) => setTitle(e.target.value)} />
           <br />
           {title}
         </div>
