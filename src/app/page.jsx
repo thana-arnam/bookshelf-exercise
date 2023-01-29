7b22c1ecdeb07280667363ec610977cab2944c1f
thanamat <thanamat@odds.team>
2023-01-29 08:15:47 +0000

    Use state books

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,12 @@
+"use client";
 import AddBookForm from "@/components/AddBookForm";
 import BookList from "@/components/BookList";
+import React from "react";
 
 const HomePage = () => {
-  const books = [{ title: "Clean Code" }];
+  // const books = [{ title: "Clean Code" }];
+  const [books, setBooks] = React.useState([{ title: "Clean Code" }]);
+
   return (
     <div>
       <AddBookForm />
